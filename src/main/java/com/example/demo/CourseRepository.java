83f7eb030b3a87977f468c9549a7e179a7db6e4b
Katie Carlson <katiecarlson@Mac.lan>
2026-06-24 09:12:09 -0700

    Final fix for CourseRepository interface

--- src/main/java/com/example/demo/CourseRepository.java
+++ src/main/java/com/example/demo/CourseRepository.java
@@ -6,13 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
-}
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course, String> {
+    // This is the only definition you need.
 }
