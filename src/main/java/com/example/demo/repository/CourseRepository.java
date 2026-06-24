81da4da63368e636cea9fe079391377ce5ccb106
Katie Carlson <katiecarlson@Mac.lan>
2026-06-24 09:18:50 -0700

    Update Repository ID type and ensure single file definition

--- src/main/java/com/example/demo/repository/CourseRepository.java
+++ src/main/java/com/example/demo/repository/CourseRepository.java
@@ -1,6 +1,9 @@
 package com.example.demo.repository;
-import com.example.demo.model.Course;
+
+import com.example.demo.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 @Repository
-public interface CourseRepository extends JpaRepository<Course, Long> {}
+public interface CourseRepository extends JpaRepository<Course, String> {
+}
