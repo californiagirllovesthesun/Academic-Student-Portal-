68acc93b44edad580af8a1f1c34f4fc682b6706a
Katie Carlson <katiecarlson@Mac.lan>
2026-06-25 09:02:19 -0700

    Remove conflicting /student mapping from all controllers

--- src/main/java/com/example/demo/controller/PortalController.java
+++ src/main/java/com/example/demo/controller/PortalController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PortalController {
     
-    @GetMapping("/instructor")
+    @GetMapping("/portal-instructor")
     public String instructor() {
         // This maps the URL /instructor to your static file
         return "forward:/portal/instructor.html"; 
     }
 
-    @GetMapping("/student")
+    @GetMapping("/portal-of-student")
     public String student() {
         // You can add one for your student page too!
         return "forward:/portal/student.html"; 
