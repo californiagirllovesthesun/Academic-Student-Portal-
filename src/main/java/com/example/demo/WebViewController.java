fe62cada26baeb96a07e6ac792b72307f9ece2a4
Katie Carlson <katiecarlson@Mac.lan>
2026-06-25 09:38:13 -0700

    Remove duplicate controllers from root package

--- src/main/java/com/example/demo/WebViewController.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo;
-
-// This controller has been disabled because PortalController handles active routing.
-public class WebViewController {
-    // Dormant file to prevent ambiguous mapping collisions
-}
